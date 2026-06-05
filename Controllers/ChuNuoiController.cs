9f11bfe524ca248617fb129193542099176edc6f
KHANHSKY0808 <khanhvipvlxx@gmail.com>
2026-06-05 08:55:45 +0700

    thêm chủ nuôi, với các trang partial của chủ nuôi

--- Controllers/ChuNuoiController.cs
+++ Controllers/ChuNuoiController.cs
@@ -7,8 +7,7 @@ namespace PetCareManagement.Controllers // Thay PetCareManagement bằng tên pr
         // GET: /ChuNuoi/ hoặc /ChuNuoi/Index
         public IActionResult Index()
         {
-            // Tạm thời chúng ta trả về giao diện trước để test đường dẫn
-            // Sau này bạn sẽ viết code gọi Database (lấy danh sách chủ nuôi) ở đây
+            // mẫu thử hiển thị view 
             return View();
         }
     }
